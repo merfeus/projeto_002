0880da58bc44897c1722517c604e9e265af0b352
Mateus Morais <85200990+merfeus@users.noreply.github.com>
2021-08-25 21:41:47 -0300

    da dando erro no repository, nullpoint

---
+++ app/src/main/java/com/example/projeto_002/repository/GitHubRepository.kt
@@ -0,0 +1,36 @@
+package com.example.projeto_002.repository
+
+import android.content.Context
+import com.example.projeto_002.database.AppDataBase
+import com.example.projeto_002.service.RetrofitBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class GitHubRepository(private val context: Context) {
+
+    private val dataBase = AppDataBase.getDataBase(context)
+    val service = RetrofitBuilder.getGitHubService()
+
+    fun fecthAll(onComplete: (GitHubResponse?, String?) -> Unit) {
+        val call = service.getAllRepo()
+        call.enqueue(object : Callback<GitHubResponse> {
+            override fun onResponse(
+                call: Call<GitHubResponse>,
+                response: Response<GitHubResponse>
+            ) {
+                if (response.body() != null) {
+                    onComplete(response.body(), null)
+                } else {
+                    onComplete(null, "Nenhum Repositorio Encontrado")
+                }
+            }
+
+            override fun onFailure(call: Call<GitHubResponse>, t: Throwable) {
+                onComplete(null, t.message)
+            }
+        })
+
+    }
+}
+
